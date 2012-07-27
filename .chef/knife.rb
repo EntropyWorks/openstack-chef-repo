c623101255d4f8a1a0dec3902300c80451bbad7d
Yazz Atlas <yazz.atlas@hp.com>
2012-07-27 12:28:31 -0700

    modified to let me set alternatives via environment vars

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,13 +1,14 @@
 raise "You must set the ORGNAME environment variable" if ENV['ORGNAME'].nil?
 current_dir = File.dirname(__FILE__)
 user = ENV['OPSCODE_USER'] || ENV['USER']
+url = ENV['CHEF_SERVER_URL'] || "https://api.opscode.com/organizations"
 log_level                :info
 log_location             STDOUT
 node_name                user
-client_key               "#{ENV['HOME']}/.chef/#{user}.pem"
+client_key               "#{ENV['HOME']}/.chef/#{user}-#{ENV['ORGNAME']}.pem"
 validation_client_name   "#{ENV['ORGNAME']}-validator"
 validation_key           "#{ENV['HOME']}/.chef/#{ENV['ORGNAME']}-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/#{ENV['ORGNAME']}"
+chef_server_url          "#{ENV['CHEF_SERVER_URL']}/#{ENV['ORGNAME']}"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
